146678379cf5588d991b89a83c553cc9b34f5e1d
marindimitrov12 <63950527+marindimitrov12@users.noreply.github.com>
2022-12-11 15:37:56 +0200

    Add service registration classes in the infrastructure projects

---
+++ HR.LeaveManagement.Identity/IdentityServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Identity
+{
+    public static class IdentityServiceRegistration
+    {
+        public static IServiceCollection ConfigureIdentityServices(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<LeaveManagementIdentityDbContext>(options=>
+            options.UseSqlServer(configuration.GetConnectionString("LeaveManagementIdentityConnectionString")));
+            return services;
+        }
+    }
+}
---
+++ HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<LeaveManagementDbContext>(options =>
+               options.UseSqlServer(
+                   configuration.GetConnectionString("LeaveManagementConnectionString")));
+            return services;
+        }
+    }
+}
