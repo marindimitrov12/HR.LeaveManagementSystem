11c30a48d2ffb492515b717942b7bb1dcafec2bc
marindimitrov12 <63950527+marindimitrov12@users.noreply.github.com>
2022-12-18 22:44:22 +0200

    fix aprove status bug

--- HR.LeaveManagement.MVC/MappingProfile.cs
+++ HR.LeaveManagement.MVC/MappingProfile.cs
@@ -11,14 +11,14 @@ namespace HR.LeaveManagement.MVC
             CreateMap<CreateLeaveTypeDto, CreateLeaveTypeVM>().ReverseMap();
             CreateMap<CreateLeaveRequestDto, CreateLeaveRequestVM>().ReverseMap();
             CreateMap<LeaveRequestDto, LeaveRequestVM>()
-               // .ForMember(q => q.DateRequested, opt => opt.MapFrom(x => x.DateRequested.DateTime))
-               // .ForMember(q => q.StartDate, opt => opt.MapFrom(x => x.StartDate.DateTime))
-               // .ForMember(q => q.EndDate, opt => opt.MapFrom(x => x.EndDate.DateTime))
+               .ForMember(q => q.DateRequested, opt => opt.MapFrom(x => x.DateRequested))
+                .ForMember(q => q.StartDate, opt => opt.MapFrom(x => x.StartDate))
+                .ForMember(q => q.EndDate, opt => opt.MapFrom(x => x.EndDate))
                 .ReverseMap();
             CreateMap<LeaveRequestListDto, LeaveRequestVM>()
-               // .ForMember(q => q.DateRequested, opt => opt.MapFrom(x => x.DateRequested.DateTime))
-               // .ForMember(q => q.StartDate, opt => opt.MapFrom(x => x.StartDate.DateTime))
-               // .ForMember(q => q.EndDate, opt => opt.MapFrom(x => x.EndDate.DateTime))
+                .ForMember(q => q.DateRequested, opt => opt.MapFrom(x => x.DateRequested))
+                .ForMember(q => q.StartDate, opt => opt.MapFrom(x => x.StartDate))
+                .ForMember(q => q.EndDate, opt => opt.MapFrom(x => x.EndDate))
                 .ReverseMap();
             CreateMap<LeaveTypeDto, LeaveTypeVM>().ReverseMap();
             CreateMap<LeaveAllocationDto, LeaveAllocationVM>().ReverseMap();
--- HR.LeaveManagement.MVC/Models/LeaveRequestVM.cs
+++ HR.LeaveManagement.MVC/Models/LeaveRequestVM.cs
@@ -14,7 +14,7 @@ namespace HR.LeaveManagement.MVC.Models
             public DateTime DateActioned { get; set; }
 
             [Display(Name = "Approval State")]
-            public bool Approved { get; set; }
+            public bool? Approved { get; set; }
 
             public bool Cancelled { get; set; }
             public LeaveTypeVM? LeaveType { get; set; }
